ce8b4517937e06a0a4a46bde367f95db9d6e294c
cfraknovich <carsonfrankovich6@gmail.com>
2021-10-04 12:30:54 -0400

    Adding movement

--- Main.py
+++ Main.py
@@ -7,37 +7,105 @@ import pygame
 from pygame.locals import *
 
 
-pygame.init()
-pygame.display.set_caption('29 Days Out')
-running = True
-FPS = 60
-WIDTH = 680	 
-HEIGHT = 480 
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.NOFRAME , 32)
-display = pygame.Surface((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-BGCOLOR = (19, 27, 35)
+def loadasset(name):
+	asset = pygame.image.load(f'{GAMEPATH}/Assets/{name}.png').convert()
+	asset.set_colorkey((0, 0, 0)) # removing black bg
+	return asset;
 
 
 def render():
 	display.fill(BGCOLOR)
+	player.render()
 
-
-def events():
+bruh = 0
+def tick(deltatime):
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			running = False
 			pygame.quit()
 			sys.exit()
+		pressed = pygame.key.get_pressed()
+		global bruh
+		print(f'blah: {bruh}')
+		bruh += 1
+		if pressed[K_w]:
+			player.vely -= 0.3
+		if pressed[K_s]:
+			player.vely += 0.3
+		if pressed[K_a]:
+			player.velx -= 0.3
+		if pressed[K_d]:
+			player.velx += 0.3
+
+	player.update(deltatime)
+
+
+class Player:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.velx = 0
+		self.vely = 0
+		self.sprite = loadasset('tempcharacter')
+
+	def update(self, dt):
+		# This looks horrible #
+		maxspeed = 0.5
+		if self.vely > maxspeed:
+			self.vely = maxspeed
+		elif self.vely < -maxspeed:
+			self.vely = -maxspeed
+		self.y = self.y + (dt * self.vely)
+
+		if self.velx > maxspeed:
+			self.velx = maxspeed
+		elif self.velx < -maxspeed:
+			self.velx = -maxspeed
+		self.x = self.x + (dt * self.velx)
+
+
+		if self.vely != 0 and abs(self.vely) > 0.0005:
+			if self.vely > 0:
+				self.vely = self.vely - 0.01
+			elif self.vely < 0:
+				self.vely = self.vely + 0.01
+		else:
+			self.vely = 0
+
+		if self.velx != 0 and abs(self.velx) > 0.0005:
+			if self.velx > 0:
+				self.velx = self.velx - 0.01
+			elif self.velx < 0:
+				self.velx = self.velx + 0.01
+		else:
+			self.velx = 0
+		#print(self.velx)
+
+	def render(self):
+		display.blit(self.sprite, (self.x, self.y))
+
+
+pygame.init()
+pygame.display.set_caption('Flash Card Thing')
+running = True
+FPS = 60
+WIDTH = 680	 
+HEIGHT = 480 
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.NOFRAME, 32)
+display = pygame.Surface((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+BGCOLOR = (19, 27, 35)
+GAMEPATH = sys.path[0]
+player = Player(100, 100)
 
 
 def main():
 	while running:
+		deltatime = clock.tick(FPS)
+		tick(deltatime)
 		render()
-		events()
 		screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
 		pygame.display.update()
-		clock.tick(FPS)
 
 
 if __name__ == '__main__':
