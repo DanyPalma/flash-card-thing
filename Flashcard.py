578ae58741499f5e209dc75a44f2da9f1b2ff260
cfraknovich <carsonfrankovich6@gmail.com>
2021-10-10 08:52:48 -0400

    Fixing issues #11 and #12

--- Flashcard.py
+++ Flashcard.py
@@ -2,18 +2,28 @@
 
 import sys
 import Utils as u
+from os import listdir
 
 entities = [] 
 pressed = None
 cardlist = []
 
+def backbtn():
+	return 1
+
+def createsetbtn():
+	return 3
+
 
 def init(pg, plr, ent):
 	global entities
 	global cardlist
-	addset = ent.Entity('blank', 1024, 8, 107, 52, pg, None)
+	addset = ent.Entity('blank', 1024, 8, 107, 52, pg, createsetbtn)
+	back = ent.Entity('blank', 104, 7, 187, 59, pg, backbtn)
 	entities.append(addset)
-	# issue 11 
+	entities.append(back)
+	files = listdir(sys.path[0])
+	cardlist = [f for f in files if f.__contains__('.json')]
 
 
 def update(pygame, display, deltatime, cs):
@@ -29,14 +39,11 @@ def update(pygame, display, deltatime, cs):
 	display.fill((19, 27, 35))
 	display.blit(u.ASSETS[2], (0, 0))
 	for e in entities:
-		e.render(display)
-
-	if pygame.mouse.get_pressed() == (1, 0, 0):
-		e = entities[0]
-		mpos = pygame.mouse.get_pos()
-		temp = pygame.Rect(mpos[0], mpos[1], 10, 10)
-		if e.istouching(temp):
-			return 3
+		if pygame.mouse.get_pressed() == (1, 0, 0):
+			mpos = pygame.mouse.get_pos()
+			temp = pygame.Rect(mpos[0], mpos[1], 10, 10)
+			if e.istouching(temp):
+				return e.activate()
 
 	return cs
 
