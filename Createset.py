a46d6f6b7ba61cf519707fb1746e1d261222057f
cfraknovich <carsonfrankovich6@gmail.com>
2021-10-10 02:06:09 -0400

    Added ability to create cards

--- Createset.py
+++ Createset.py
@@ -7,31 +7,113 @@ import Textbox as tb
 entities = [] 
 pressed = None
 boxes = []
+cards = []
+currentcard = 0
+
+
+class Card:
+	def __init__(self, front, back):
+		self.front = front
+		self.back = back
+
+	def setfront(self, newfront):
+		self.front = newfront
+		
+	def setback(self, newback):
+		self.back = newback
+
+	def getfront(self):
+		return self.front
+
+	def getback(self):
+		return self.back
+
+
+def done():
+	try: 
+		temp = cards[currentcard]
+	except:
+		create()
+
+	# create a json file containing everything #
+	if boxes[0].text == '':
+		return
+	f = open(f'{boxes[0].text}.json', 'w')
+	f.write('[\n')
+	for x, card in enumerate(cards):
+		print(x)
+		f.write('\t{ "Front" : "' + card.front + '",\n')	
+		if x == len(cards)-1:
+			f.write('\t  "Back" : "' + card.back + '" }\n\n')	
+		else:
+			f.write('\t  "Back" : "' + card.back + '" },\n\n')	
+
+	f.write(']')
+	return 1
+
+	
+def right():
+	global currentcard
+	try: 
+		temp = cards[currentcard]
+	except:
+		create()
+
+	currentcard += 1
+	try: 
+		boxes[1].text = cards[currentcard].front
+		boxes[2].text = cards[currentcard].back
+	except:
+		boxes[1].text = ''
+		boxes[2].text = ''
+
+
+def left():
+	global currentcard
+	if currentcard == 0:
+		return
+	try:
+		temp = cards[currentcard]
+	except:
+		create()
+
+	currentcard -= 1
+	boxes[1].text = cards[currentcard].front
+	boxes[2].text = cards[currentcard].back
+
+
+def create():
+	global cards
+	newcard = Card(boxes[1].text, boxes[2].text)
+	cards.append(newcard)
+
+
+def delete():
+	global cards
+	cards.pop(currentcard)
+	boxes[1].text = cards[currentcard].front
+	boxes[2].text = cards[currentcard].back
+
 
 def init(pg, plr, ent):
 	global entities
 	global boxes
-	entities.append(ent.Entity('blank', 841, 25, 248, 72, pg, None))
+	entities.append(ent.Entity('blank', 841, 25, 248, 72, pg, done))
 	entities.append(ent.Entity('blank', 448, 576, 128, 128, pg, None))
-	entities.append(ent.Entity('blank', 704, 576, 128, 128, pg, None))
-	entities.append(ent.Entity('blank', 192, 576, 128, 128, pg, None))
-	entities.append(ent.Entity('blank', 960, 576, 128, 128, pg, None))
+	entities.append(ent.Entity('blank', 704, 576, 128, 128, pg, delete))
+	entities.append(ent.Entity('blank', 192, 576, 128, 128, pg, left))
+	entities.append(ent.Entity('blank', 960, 576, 128, 128, pg, right))
 
-	boxes.append(tb.Textbox(pg, 192, 18, 536, 79, 'fart', (255, 255, 255), 32))
-	boxes.append(tb.Textbox(pg, 64, 192, 512, 320, 'fart2', (255, 255, 255), 32))
-	boxes.append(tb.Textbox(pg, 704, 192, 512, 320, 'fart3', (255, 255, 255), 32))
+	boxes.append(tb.Textbox(pg, 192, 18, 536, 79, '', (255, 255, 255), 32))
+	boxes.append(tb.Textbox(pg, 64, 192, 512, 320, '', (255, 255, 255), 32))
+	boxes.append(tb.Textbox(pg, 704, 192, 512, 320, '', (255, 255, 255), 32))
 
 
 def update(pygame, display, deltatime, cs):
 	# tick
-	global text
 	pressed = pygame.key.get_pressed()
-	if pressed[pygame.K_BACKSPACE]:
-		pass
-	for event in pygame.event.get():
-		if event.type == pygame.KEYDOWN:
-			if event.key != pygame.K_BACKSPACE and event.key != pygame.K_RETURN:
-				pass
+	events = pygame.event.get()
+	for event in events:
 		if event.type == 256:
 			pygame.quit()
 			sys.exit()
@@ -40,13 +122,33 @@ def update(pygame, display, deltatime, cs):
 	display.fill((19, 27, 35))
 	display.blit(u.ASSETS[4], (0, 0))
 
+	#sur = lolfont.render(f'Card {currentcard+1} out of {len(cards)}', True, (255, 255, 255))
+
+	# fix this. yuck #
 	for box in boxes:
 		box.render(pygame, display)
+		if box.active:
+			for event in events:
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_BACKSPACE:
+						box.text = box.text[0:-1]
+					else:
+						box.text += event.unicode
 		if pygame.mouse.get_pressed() == (1, 0, 0):
+			box.turnoff()
 			mpos = pygame.mouse.get_pos()
 			temp = pygame.Rect(mpos[0], mpos[1], 10, 10)
 			if box.istouching(temp):
-				box.toggle()
+				box.turnon()
+	
+	for e in entities:
+		for event in events:
+			if event.type == pygame.MOUSEBUTTONUP:
+				mpos = pygame.mouse.get_pos()
+				temp = pygame.Rect(mpos[0], mpos[1], 10, 10)
+				if e.istouching(temp):
+					if e.activate() == 1:
+						return 2
 
 	return cs
 
--- Textbox.py
+++ Textbox.py
@@ -7,18 +7,22 @@ class Textbox:
 		self.text = text
 		self.hitbox = pg.Rect(x, y, w, h)
 		self.color = color
-		font = pg.font.Font(None, fontsize) 
-		self.sur = font.render(text, True, color)
+		self.font = pg.font.Font(None, fontsize) 
 		self.active = False
 
 	def render(self, pygame, display):
+		sur = self.font.render(self.text, True, self.color)
 		pygame.draw.rect(display, self.color, self.hitbox, 1)
-		display.blit(self.sur, self.hitbox)
+		display.blit(sur, self.hitbox)
 		
 	def istouching(self, hitbox2):
 		return hitbox2.colliderect(self.hitbox) 
 
-	def toggle(self):
-		#self.color = (0, 255, 0)
-		self.active = not self.active
+	def turnon(self):
+		self.active = True
+		self.color = (0, 255, 0)
+
+	def turnoff(self):
+		self.active = False 
+		self.color = (255, 0, 0)
 
